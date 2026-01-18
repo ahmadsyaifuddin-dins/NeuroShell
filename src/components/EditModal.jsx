79dba637c2fab47108663f43af0a08f2978d6170
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2026-01-18 13:23:00 +0800

    3 menit

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -65,6 +65,7 @@ export default function EditModal({ project, onClose, onSave }) {
                             className="w-full bg-slate-950 border border-neuro-green/30 p-2 text-neuro-green focus:border-neuro-green focus:outline-none text-xs font-mono rounded"
                         >
                             <option value="0" className='text-red-600'>OFF (Realtime - Aggressive)</option>
+                            <option value="3">3 Minutes</option>
                             <option value="5">5 Minutes (Balanced)</option>
                             <option value="10">10 Minutes</option>
                             <option value="15">15 Minutes</option>
